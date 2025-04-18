d2bdf5389d0f8a0a991a8f9ec8b4acc80339729b
Ramzy Kermad <kermad.r@sncf.fr>
2025-04-18 18:49:30 +0200

    Updated gradle sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.testappmoc3a"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.testappmoc3a"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
